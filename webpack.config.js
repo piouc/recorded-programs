319f44a8abfdc4f83e4470e3d335f9ab55878df4
piou <piou@hotmail.co.jp>
2018-08-15 10:52:15 +0900

    Use npm script and webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+
+module.exports = {
+	entry: {
+		index: './src/js/index.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	}
+}
